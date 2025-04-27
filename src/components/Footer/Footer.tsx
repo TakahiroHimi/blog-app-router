fa539091cdd26f780db80e96ab37bdb9a884045c
TakahiroHimi <takahiro.mth.1209@gmail.com>
2025-04-27 09:51:33 +0900

    スタイル調整

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 
 export function Footer() {
   return (
-    <footer className="py-6 border-t border-gray-200 bg-gray-50">
-      <div className="container mx-auto px-4 max-w-4xl">
+    <footer className="py-6 px-4 border-t border-gray-200 bg-gray-50">
+      <div className="container mx-auto max-w-4xl">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
             <Link href="/" className="text-lg font-medium">
