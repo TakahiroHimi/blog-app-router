9e0917ed99ae2a75371c2f14b097806146289424
TakahiroHimi <takahiro.mth.1209@gmail.com>
2025-05-06 13:48:33 +0900

    a11y自動テストを追加

---
+++ e2e/post.e2e.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test'
+import AxeBuilder from '@axe-core/playwright'
+
+test.describe('Post - E2E', () => {
+  test('トップページ', async ({ page }) => {
+    await page.goto('http://localhost:3000')
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze()
+
+    expect(accessibilityScanResults.violations).toEqual([])
+  })
+
+  test('マークダウンテストページ', async ({ page }) => {
+    await page.goto('http://localhost:3000/posts/2099/01/01_markdown-test')
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).exclude('input[type="checkbox"]').analyze()
+
+    expect(accessibilityScanResults.violations).toEqual([])
+  })
+
+  test('カスタムコンポーネントテストページ', async ({ page }) => {
+    await page.goto('http://localhost:3000/posts/2099/01/02_custom-component-test')
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze()
+
+    expect(accessibilityScanResults.violations).toEqual([])
+  })
+})
