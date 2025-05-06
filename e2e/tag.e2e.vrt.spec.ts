f83c91bec031c265f86205452976cf39860c435a
TakahiroHimi <takahiro.mth.1209@gmail.com>
2025-05-06 18:43:30 +0900

    不要なテストを削除

--- e2e/tag.e2e.vrt.spec.ts
+++ e2e/tag.e2e.vrt.spec.ts
@@ -1,58 +1,9 @@
 import { test, expect } from '@playwright/test'
 
 test.describe('Tag - VRT', () => {
-  test('タグ「テスト」ページ', async ({ page, viewport }) => {
+  test('タグ「テスト」ページ', async ({ page }) => {
     await page.goto('/tags/テスト')
 
-    if (!viewport) {
-      throw new Error('Viewport is not defined')
-    }
-
-    await page.setViewportSize({ width: viewport.width, height: 4000 })
-    await expect(page).toHaveScreenshot()
-  })
-
-  test('タグ「マークダウン」ページ', async ({ page, viewport }) => {
-    await page.goto('/tags/マークダウン')
-
-    if (!viewport) {
-      throw new Error('Viewport is not defined')
-    }
-
-    await page.setViewportSize({ width: viewport.width, height: 4000 })
-    await expect(page).toHaveScreenshot()
-  })
-
-  test('タグ「カスタムコンポーネント」ページ', async ({ page, viewport }) => {
-    await page.goto('/tags/カスタムコンポーネント')
-
-    if (!viewport) {
-      throw new Error('Viewport is not defined')
-    }
-
-    await page.setViewportSize({ width: viewport.width, height: 4000 })
-    await expect(page).toHaveScreenshot()
-  })
-
-  test('タグ「タグ機能」ページ', async ({ page, viewport }) => {
-    await page.goto('/tags/タグ機能')
-
-    if (!viewport) {
-      throw new Error('Viewport is not defined')
-    }
-
-    await page.setViewportSize({ width: viewport.width, height: 4000 })
-    await expect(page).toHaveScreenshot()
-  })
-
-  test('タグ「プログラミング」ページ', async ({ page, viewport }) => {
-    await page.goto('/tags/プログラミング')
-
-    if (!viewport) {
-      throw new Error('Viewport is not defined')
-    }
-
-    await page.setViewportSize({ width: viewport.width, height: 4000 })
-    await expect(page).toHaveScreenshot()
+    await expect(page).toHaveScreenshot({ fullPage: true })
   })
 })
