faac13fa61cebefc6277e6a6de220397be048533
TakahiroHimi <takahiro.mth.1209@gmail.com>
2025-05-25 19:29:54 +0900

    awaitを追加

--- e2e/post.e2e.spec.ts
+++ e2e/post.e2e.spec.ts
@@ -9,20 +9,20 @@ test.describe('Post - E2E', () => {
     const link1 = page.getByRole('link', {
       name: '予備のテスト用ページ 予備のテスト用ページです。一般ユーザーには表示されません。 テスト環境で「他の記事も見る」セクションの表示を一定にするために作成しています',
     })
-    expect(link1).toBeVisible()
-    expect(link1).toHaveAttribute('href', '/posts/2099/01/04_buff-test')
+    await expect(link1).toBeVisible()
+    await expect(link1).toHaveAttribute('href', '/posts/2099/01/04_buff-test')
 
     const link2 = page.getByRole('link', {
       name: 'タグテスト用ページ このページはタグ機能のテストのために作成された特別なページです。一般ユーザーには表示されません。 タグ機能のテスト このページには「テスト」',
     })
-    expect(link2).toBeVisible()
-    expect(link2).toHaveAttribute('href', '/posts/2099/01/03_tag-test')
+    await expect(link2).toBeVisible()
+    await expect(link2).toHaveAttribute('href', '/posts/2099/01/03_tag-test')
 
     const link3 = page.getByRole('link', {
       name: 'カスタムコンポーネントテスト用ページ このページはカスタムコンポーネントのテストのために作成された特別なページです。一般ユーザーには表示されません。',
     })
-    expect(link3).toBeVisible()
-    expect(link3).toHaveAttribute('href', '/posts/2099/01/02_custom-component-test')
+    await expect(link3).toBeVisible()
+    await expect(link3).toHaveAttribute('href', '/posts/2099/01/02_custom-component-test')
   })
 
   test.describe('マークダウンテストページ', () => {
